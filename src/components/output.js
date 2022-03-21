cdbc394e043fb5299d6aceee0fa2b2460d358f77
devlettum <batuhanarik123@gmail.com>
2022-03-21 22:43:38 +0300

    Finished.

---
+++ src/components/output.js
@@ -0,0 +1,28 @@
+
+
+function Output(props) {
+
+    console.log(props);
+    console.log(props.InputInfos.surname);
+
+        return (
+            <div>
+            
+                <p>{props.InputInfos.name}</p>
+                <p>{props.InputInfos.surname}</p>
+                <p>{props.InputInfos.birthday}</p>
+                <p>{props.InputInfos.describe}</p>
+                <p>AAA</p>
+                <p>AAA</p>
+                <p>AAA</p>
+                <p>AAA</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto doloremque eos explicabo molestias quis. A asperiores blanditiis cumque deleniti dignissimos dolore ea et eveniet, excepturi fugit id iste nisi veniam.</p>
+                <p>AAAAAAAAAAAAA</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto doloremque eos explicabo molestias quis. A asperiores blanditiis cumque deleniti dignissimos dolore ea et eveniet, excepturi fugit id iste nisi veniam.</p>
+                <p>wlkjklwqjklqwjwq.com</p>
+            </div>
+        );
+    }
+
+
+export default Output;
