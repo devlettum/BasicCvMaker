ee069806d7d802c6b2b847170aeb01a11a2832fb
devlettum <batuhanarik123@gmail.com>
2022-03-22 00:41:43 +0300

    Finished

--- src/components/output.js
+++ src/components/output.js
@@ -1,7 +1,8 @@
 function Output(props) {
  
     return (
-      <div>
+      <div style={{ marginTop:100,marginLeft:100}}>
+          <h2 style={{ color: 'gray',textAlign: 'center' }}>CV'niz hazırlandı...</h2>
         <div>Name : {props.InputInfos.name}</div>
         <div>Surname : {props.InputInfos.surname}</div>
         <div>Birthday : {props.InputInfos.birthday}</div>
